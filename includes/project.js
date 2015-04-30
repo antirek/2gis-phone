d144f8a4b0eaae4c732cc5f3e76c880382269d0d
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-05-01 06:48:14 +0700

    git fix'

--- includes/project.js
+++ includes/project.js
@@ -75,10 +75,10 @@ $().ready(function(){
         $("#results").html("");
         //console.log(search)
 
-        /*
+        
         var url = 'http://catalog.api.2gis.ru/search?what=' + 
             search + '&where=Красноярск&version=1.3&key=rusjdw2920';
-        */
+        
 
         var url = 'data.json';
     
